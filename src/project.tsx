cd871fc1fd654c57da74562ae6d0348ad7f8f704
Dev-wonderful <adeonederful20@gmail.com>
2022-09-10 03:24:16 +0100

    project preview and links

--- src/project.tsx
+++ src/project.tsx
@@ -1,21 +1,73 @@
 import React from "react";
 import project from './project.module.css'
-import { getLinkPreview } from 'link-preview-js'
-
+import image from './assets/LInkedin.jpg'
 interface ProjectProps {
     
 }
  
 const Project: React.FunctionComponent<ProjectProps> = () => {
-    // getLinkPreview("https://gds-foundation.herokuapp.com").then(data => console.log(data))
+    
+    interface Preview {
+        title: string,
+        description: string,
+        image: string,
+        url: string
+    }
+    const preview: Preview[] = [
+        {
+            "title": "GDSFoundation",
+            "description": "To Provide Spiritual and Emotional Support to Widows and Single Mothers through the Preaching of the Gospel.",
+            "image": "https://gds-foundation.herokuapp.com/.public/logo.png",
+            "url": "https://gds-foundation.herokuapp.com"
+          },
+          {
+            "title": "Product Page",
+            "description": "",
+            "image": "",
+            "url": "https://wonder-shopping.netlify.app/"
+          },
+          {
+            "title": "Dev-wonderful - Repositories",
+            "description": "Technically Adventurous but......... Dev-wonderful has 26 repositories available. Follow their code on GitHub.",
+            "image": "https://avatars.githubusercontent.com/u/69244197?v=4?s=400",
+            "url": "https://github.com/Dev-wonderful?tab=repositories"
+          },
+          {
+            "title": "Wonderful Adeyemo - Frontend Web Developer - Freelance | LinkedIn",
+            "description": "View Wonderful Adeyemo’s profile on LinkedIn, the world’s largest professional community. Wonderful has 2 jobs listed on their profile. See the complete profile on LinkedIn and discover Wonderful’s connections and jobs at similar companies.",
+            "image": "https://media-exp1.licdn.com/dms/image/C4D03AQFIGyGvDnG75A/profile-displayphoto-shrink_200_200/0/1639677794166?e=2147483647\u0026v=beta\u0026t=Rh66imporW5txnB2uUb79Y4Nsak-0i8YLDfk9DZ8ZVM",
+            "url": "https://www.linkedin.com/in/wonderful-adeyemo-4789121a3"
+          }
+    ]
+    
     return ( 
         <section className={project.container}>
             <h2 className={project.heading}>Projects</h2>
             <div className={project.section}>
-                <div className={project.card}><a href="https://gds-foundation.herokuapp.com">A Foundation website</a></div>
-                <div className={project.card}><a href="https://wonder-shopping.netlify.app/">An E-commerce Page</a></div>
-                <div className={project.card}><a href="https://github.com/Dev-wonderful?tab=repositories">My Github Repo</a></div>
-                <div className={project.card}><a href="https://linkedin.com/in/wonderful-adeyemo-4789121a3">My LinkedIn Profile</a></div>
+                <div className={project.card}>
+                    <div className={project.imagediv}><img src="https://gds-foundation.herokuapp.com/public/logo.png" alt="" /></div>
+                    <h4>GDSFoundation</h4>
+                    <p>To Provide Spiritual and Emotional Support to Widows and Single Mothers through the Preaching of the Gospel.</p>
+                    <a href="https://gds-foundation.herokuapp.com">The Foundation website</a>
+                </div>
+                <div className={project.card}>
+                    <div className={project.imagediv}><img src="" alt="Shopping Cart" /></div>
+                    <h4>Product Page</h4>
+                    <p>It has the ability to track orders through the add and remove from cart functionality, quantity and price calculation</p>
+                    <a href="https://wonder-shopping.netlify.app/">An E-commerce Page</a>
+                </div>
+                <div className={project.card}>
+                    <div className={project.imagediv}><img src="https://avatars.githubusercontent.com/u/69244197?v=4?s=400" alt="Github Profile Picture" /></div>
+                    <h4>Dev-wonderful - Repositories</h4>
+                    <p>Technically Adventurous but......... Dev-wonderful has 26 repositories available. Follow his code on GitHub.</p>
+                    <a href="https://github.com/Dev-wonderful?tab=repositories">My Github Repo</a>
+                </div>
+                <div className={project.card}>
+                    <div className={project.imagediv}><img src={image} alt="LinkedIn Profile Picture" /></div>
+                    <h4>Wonderful Adeyemo - Frontend Web Developer - Freelance | LinkedIn</h4>
+                    <p>Wonderful has 2 jobs listed on their profile. See the complete profile on LinkedIn and discover Wonderful’s connections and jobs at similar companies.</p>
+                    <a href="https://linkedin.com/in/wonderful-adeyemo-4789121a3">My LinkedIn Profile</a>
+                </div>
             </div>
         </section>
      );
